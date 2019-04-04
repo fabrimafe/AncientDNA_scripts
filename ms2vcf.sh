2b37f3f98e572ade15e769ac074c3edc98a0c780
fabrizio <fabrizio.mafessoni@gmail.com>
2019-04-04 11:41:03 +0200

    .

--- ms2vcf.sh
+++ ms2vcf.sh
@@ -2,14 +2,9 @@
 
 source ~/Dropbox/Vindija/scripts/myfunctions.sh --source-only
 
-POP=$1
-i=$2
-FOLDERFIT=$3
-MYL=$4
-mkdir -p $FOLDERFIT
-zcat /mnt/454/Vindija/high_cov/genotypes/Altai/all_sites_VCF/chr${i}_mq25.vcf.gz | head -100 | grep '#' > ${FOLDERFIT}/${POP}.chr${i}.vcf
-cat ${FOLDERFIT}/chr${i}.${POP}.ms.res | awk -v myl=${MYL} -v mychr=${i} -v FS=' ' -v OFS='\t' '{if (NR==6){for (i=2;i<=NF;i++){print mychr,int($i*myl)+1,".","A","T",50,".",".","GT:DP:A:C:G:T:PP:GQ","0/1"}}}' >> ${FOLDERFIT}/${POP}.chr${i}.vcf #cat Altai.ms.res #+1 in position is just to avoid 0s
-gzip -f ${FOLDERFIT}/${POP}.chr${i}.vcf
-nohup zcat ${FOLDERFIT}/${POP}.chr${i}.vcf.gz | head -100 | grep '#' > ${FOLDERFIT}/${POP}.chr${i}.genome.vcf #filled up
-nohup zcat ${FOLDERFIT}/${POP}.chr${i}.vcf.gz | grep -v '#' | fill_vcf_with00 >> ${FOLDERFIT}/${POP}.chr${i}.genome.vcf
-gzip -f ${FOLDERFIT}/${POP}.chr${i}.genome.vcf
+INPUTFILE=$1 #input file is the output of a simulation in ms format
+OUTPUTFILE=$2 #a vcf. You the unzipped name as argument, it will be bgzipped anyway.
+
+zcat /mnt/454/Vindija/high_cov/genotypes/Altai/chr22_mq25_mapab100.vcf.gz | head -100 | grep '#' > ${OUTPUTFILE}
+cat ${INPUTFILE} | awk -v myl=${MYL} -v mychr=${i} -v FS=' ' -v OFS='\t' '{if (NR==6){for (i=2;i<=NF;i++){print mychr,int($i*myl)+1,".","A","T",50,".",".","GT:DP:A:C:G:T:PP:GQ","0/1"}}}' >> ${OUTPUTFILE} #+1 in position is just to avoid 0s
+bgzip -f ${OUTPUTFILE}
