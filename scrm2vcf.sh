c583c2a0b4c850306f3b2d72a6613d4671535946
fabrimafe <fabrizio.mafessoni@gmail.com>
2019-04-11 11:46:21 +0200

    ms & scrm outputs differ

--- scrm2vcf.sh
+++ scrm2vcf.sh
@@ -6,5 +6,5 @@ MYCHR=$3
 MYL=$4 #length of simulated genome
 
 zcat /mnt/454/Vindija/high_cov/genotypes/Altai/chr22_mq25_mapab100.vcf.gz | head -100 | grep '#' > ${OUTPUTFILE}
-cat ${INPUTFILE} | awk -v myl=${MYL} -v mychr=${MYCHR} -v FS=' ' -v OFS='\t' '{if (NR==6){for (i=2;i<=NF;i++){print mychr,int($i*myl)+1,".","A","T",50,".",".","GT:DP:A:C:G:T:PP:GQ","0/1"}}}' >> ${OUTPUTFILE} #+1 in position is just to avoid 0s
+cat ${INPUTFILE} | grep position | awk -v myl=${MYL} -v mychr=${MYCHR} -v FS=' ' -v OFS='\t' '{for (i=2;i<=NF;i++){print mychr,int($i*myl)+1,".","A","T",50,".",".","GT:DP:A:C:G:T:PP:GQ","0/1"}}' >> ${OUTPUTFILE} #+1 in position is just to avoid 0s
 bgzip -f ${OUTPUTFILE}
