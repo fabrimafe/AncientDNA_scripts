f9ddcbb6685432421f3d7232f7f2c7f9b2905202
fabrimafe <fabrizio.mafessoni@gmail.com>
2019-06-07 13:11:38 +0200

    .

--- chagyrskaya/enrich_in_bed.R
+++ chagyrskaya/enrich_in_bed.R
@@ -5,6 +5,7 @@ outfile<-as.character(args[2])
 
 
 mydata<-read.table(inputfile)
+mydata<-mydata[apply(mydata,MARGIN=1,FUN=function(x) sum(as.numeric(x[2:4]))!=0),]
 mydata0<-mydata[,2:5]
 #mydata<-mydata[mydata[,2]!=0 & mydata[,3]!=0,]
 mydata<-cbind(mydata,ratioobs=mydata0[,1]/mydata0[,2],ratioall=mydata0[,3]/mydata0[,4],chisq.pvalue=apply(mydata0,MARGIN=1, FUN=function(x) chisq.test(cbind(c(x[1],x[2]),c(x[3],x[4])))$p.value))
