be5f25d658c1e339e869ee38c4db7d36ef6900fa
fabrizio <fabrizio.mafessoni@gmail.com>
2019-10-24 13:36:19 +0200

    numerical derivatives in R

---
+++ deriv.R
@@ -0,0 +1,10 @@
+#Numerical derivative in R
+https://www.r-bloggers.com/numerical-differentiation-with-finite-differences-in-r/
+
+#also package that does it directly
+library('devtools')
+install_github('philchalmers/mirt')
+
+library("numDeriv")
+install("")
+
